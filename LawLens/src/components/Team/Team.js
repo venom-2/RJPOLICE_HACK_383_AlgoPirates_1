c26e1b240cad4d49c9b8c88891bce10cf3dab6fd
Krishna <kdwivedi343@gmail.com>
2023-12-29 14:42:17 +0530

    removed icons

--- LawLens/src/components/Team/Team.js
+++ LawLens/src/components/Team/Team.js
@@ -13,20 +13,13 @@ export default function Team() {
           <h1 className="title"></h1>
           <div className="teamGrid">
             <div xs={4} className="member">
-              <img
-                src={member1}
-                alt="Emma"
-                data-aos="fade-right"
-                data-aos-anchor-placement="center-bottom"
-                data-aos-delay="500"
-                data-aos-duration="1000"
-              />
+              
               <h3 className="memberInfo">
-                Super Admin
+                
                 <br />
-                DGP, Rajasthan Police
+                
                 <br />
-                info@police.com
+                
               </h3>
             </div>
             <div
@@ -36,31 +29,15 @@ export default function Team() {
               data-aos-delay="500"
               data-aos-duration="1000"
             >
-              <img src={member2} alt="Manesh" />
               <h3 className="memberInfo">
-                Manesh Jhawar
+                
                 <br />
-                SHO, Rajasthan Police
                 <br />
-                manesh.jhawar@police.com
               </h3>
             </div>
             <div className="member">
-              <img
-                src={member3}
-                alt="Nick"
-                data-aos="fade-left"
-                data-aos-anchor-placement="center-bottom"
-                data-aos-delay="500"
-                data-aos-duration="1000"
-              />
-              <h3 className="memberInfo">
-                Nick Zhang
-                <br />
-                IG, Nerdy kid
-                <br />
-                nzhang.jiahui@police.com
-              </h3>
+              
+              
             </div>
           </div>
         </div>
