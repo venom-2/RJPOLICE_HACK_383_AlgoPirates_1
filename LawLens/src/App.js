e5c2cbf9b96e522be9b7622da04052c7e4a6057b
Akshat Trivedi <akshattrivedi32@gmail.com>
2023-12-30 15:16:08 +0530

    minor change

--- LawLens/src/App.js
+++ LawLens/src/App.js
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import signup from "./components/Login/LoginSignup"
-import Header from "./components/Header/Header";
 import signin from "./components/Login/SignIn";
 
 function App() {
