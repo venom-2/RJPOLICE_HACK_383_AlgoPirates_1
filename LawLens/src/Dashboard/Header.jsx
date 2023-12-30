6ec196c62ae62cf8b234da67c8585eadcd8729ab
Akshat Trivedi <akshattrivedi32@gmail.com>
2023-12-30 23:42:13 +0530

    dashboard changes

--- LawLens/src/Dashboard/Header.jsx
+++ LawLens/src/Dashboard/Header.jsx
@@ -7,8 +7,11 @@ import {
   BsJustify,
 } from "react-icons/bs";
 import "./App.css";
+import { IoIosLogOut } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 function Header({ OpenSidebar }) {
+  const style={width: '35px', height: '35px' };
   return (
     <div className="header-dashboard">
       <div>
@@ -16,13 +19,16 @@ function Header({ OpenSidebar }) {
           <BsJustify className="icon" onClick={OpenSidebar} />
         </div>
         <div className="header-left">
-          <BsSearch className="icon-top" />
+          {/* <BsSearch className="icon-top" /> */}
         </div>
       </div>
       <div className="header-right">
-        <BsFillBellFill className="icon-top" />
+        {/* <BsFillBellFill className="icon-top" />
         <BsFillEnvelopeFill className="icon-top" />
-        <BsPersonCircle className="icon-top" />
+        <BsPersonCircle className="icon-top" /> */}
+        <Link to="/signin">
+          <IoIosLogOut style={style}/>
+        </Link>
       </div>
     </div>
   );
